1fd1266dc4710d709139898b85db634f8475b372
Parrandos <63514959+Parrandos@users.noreply.github.com>
2022-04-19 14:27:18 -0300

    meti la consola en clase consola

--- src/Caballo.java
+++ src/Caballo.java
@@ -1,5 +1,4 @@
 public class Caballo {
     // bounds: a-h, 1-8. a1-b1 es un mov, b1-b2 es otro
-
-
+    
 }
---
+++ src/Consola.java
@@ -0,0 +1,29 @@
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Consola {
+    public static void inputs()
+    {
+        Scanner consola = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Seleccionar una de las siguientes:\n" + "• 1: Realizar el siguiente salto.\n" +
+                "• 2: Mostrar el contenido de las pilas.\n" +
+                "• 3: Salir.");
+
+        boolean repetir = true;
+        while (repetir) {
+            String userName = consola.nextLine();  // Read user input
+            if (Objects.equals(userName, "1")) {
+                System.out.println("Elegiste la 1");  // Output user input
+                repetir = false;
+            } else if (Objects.equals(userName, "2")) {
+                System.out.println("Elegiste la 2");
+                repetir = false;
+            } else if (Objects.equals(userName, "3")) {
+                System.out.println("Elegiste la 3");
+                repetir = false;
+            } else{
+                System.out.println("Input no valido, vuelva a intentar");
+                repetir = true;}
+        }
+    }
+}
