ab46dbdcdf8a6b4d43c7505aed10f0b0fb2660e2
Parrandos <63514959+Parrandos@users.noreply.github.com>
2022-04-19 14:33:44 -0300

    .

--- src/Consola.java
+++ src/Consola.java
@@ -5,9 +5,11 @@ public class Consola {
     public static void inputs()
     {
         Scanner consola = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("Seleccionar una de las siguientes:\n" + "• 1: Realizar el siguiente salto.\n" +
-                "• 2: Mostrar el contenido de las pilas.\n" +
-                "• 3: Salir.");
+        System.out.println("""
+                Seleccionar una de las siguientes:
+                • 1: Realizar el siguiente salto.
+                • 2: Mostrar el contenido de las pilas.
+                • 3: Salir.""");
 
         //Nota parcial: no va a devolver  System.out.println("Elegiste la 1");
         // devolverá el método, aún no creado, nextMove(), o algo así.
@@ -24,8 +26,7 @@ public class Consola {
                 System.out.println("Elegiste la 3");
                 repetir = false;
             } else{
-                System.out.println("Input no valido, vuelva a intentar");
-                repetir = true;}
+                System.out.println("Input no valido, vuelva a intentar");}
         }
     }
 }
--- src/main.java
+++ src/main.java
@@ -4,5 +4,6 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
         Consola.inputs();
+        Consola.inputs();
     }
 }
